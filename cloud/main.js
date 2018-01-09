d885dcd60eec7799d3d56ce7f0c13e90e3b5bb3a
Joniwee <33837126+Joniwee@users.noreply.github.com>
2018-01-09 18:40:43 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,34 +1,25 @@
 
-// Android push test
-// To be used with:
-// https://github.com/codepath/ParsePushNotificationExample
-// See https://github.com/codepath/ParsePushNotificationExample/blob/master/app/src/main/java/com/test/MyCustomReceiver.java
-
+// Depends on this function: https://github.com/codepath/parse-server-example/blob/master/cloud/main.js
 Parse.Cloud.define('pushChannelTest', function(request, response) {
 
   // request has 2 parameters: params passed by the client and the authorized user
   var params = request.params;
   var user = request.user;
 
+  // extract out the channel to send
+  var action = params.action;
+  var message = params.message;
   var customData = params.customData;
-  var launch = params.launch;
-  var broadcast = params.broadcast;
 
   // use to custom tweak whatever payload you wish to send
   var pushQuery = new Parse.Query(Parse.Installation);
   pushQuery.equalTo("deviceType", "android");
 
-  var payload = {};
-
-  if (customData) {
-      payload.customdata = customData;
-  }
-  else if (launch) {
-      payload.launch = launch;
-  }
-  else if (broadcast) {
-      payload.broadcast = broadcast;
-  }
+  var payload = {"data": {
+      "alert": message,
+      "action": action,
+      "customdata": customData}
+                };
 
   // Note that useMasterKey is necessary for Push notifications to succeed.
 
