5c3a86abf6cd34d2092a726b58022e26efe4282d
Joniwee <33837126+Joniwee@users.noreply.github.com>
2018-01-10 00:19:07 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,26 +1,39 @@
 
-// Defined in cloud/main.js on Parse server side
-Parse.Cloud.define('pingReply', function(request, response) {
+// Depends on this function: https://github.com/codepath/parse-server-example/blob/master/cloud/main.js
+Parse.Cloud.define('pushChannelTest', function(request, response) {
+
+  // request has 2 parameters: params passed by the client and the authorized user
   var params = request.params;
+  var user = request.user;
+
+  // extract out the channel to send
+  var action = params.action;
+  var message = params.message;
   var customData = params.customData;
 
-  if (!customData) {
-    response.error("Missing customData!")
-  }
+  // use to custom tweak whatever payload you wish to send
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo("deviceType", "android");
 
-  var sender = JSON.parse(customData).sender;
-  var query = new Parse.Query(Parse.Installation);
-  query.equalTo("installationId", sender);
+  var payload = {"data": {
+      "alert": message,
+      "action": action,
+      "customdata": customData}
+                };
 
-  Parse.Push.send({
-  where: query,
-  // Parse.Push requires a dictionary, not a string.
-  data: {"alert": "The Giants scored!"},
-  }, { success: function() {
-     console.log("#### PUSH OK");
-  }, error: function(error) {
-     console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
+    // Note that useMasterKey is necessary for Push notifications to succeed.
 
-  response.success('success');
+  Parse.Push.send(
+  {
+    where: pushQuery,                                                                                                             
+    data: payload
+  }, 
+  { 
+    success: function() {console.log("#### PUSH OK");}, 
+    error: function(error) {console.log("#### PUSH ERROR" + error.message);}, 
+    useMasterKey: true
+  });
+
+
+  response.success('testSuccess');
 });
