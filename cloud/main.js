e65e3783d9b1f25e6bccc02cd28c101ea622ee7b
Joniwee <33837126+Joniwee@users.noreply.github.com>
2018-01-09 23:57:53 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,39 +1,26 @@
 
-// Depends on this function: https://github.com/codepath/parse-server-example/blob/master/cloud/main.js
-Parse.Cloud.define('pushChannelTest', function(request, response) {
-
-  // request has 2 parameters: params passed by the client and the authorized user
+// Defined in cloud/main.js on Parse server side
+Parse.Cloud.define('pingReply', function(request, response) {
   var params = request.params;
-  var user = request.user;
-
-  // extract out the channel to send
-  var action = params.action;
-  var message = params.message;
   var customData = params.customData;
 
-  // use to custom tweak whatever payload you wish to send
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo("deviceType", "android");
+  if (!customData) {
+    response.error("Missing customData!")
+  }
 
-  var payload = {"data": {
-      "alert": message,
-      "action": action,
-      "customdata": customData}
-                };
+  var sender = JSON.parse(customData).sender;
+  var query = new Parse.Query(Parse.Installation);
+  query.equalTo("installationId", sender);
 
-    // Note that useMasterKey is necessary for Push notifications to succeed.
+  Parse.Push.send({
+  where: query,
+  // Parse.Push requires a dictionary, not a string.
+  data: {"alert": "The Giants scored!"},
+  }, { success: function() {
+     console.log("#### PUSH OK");
+  }, error: function(error) {
+     console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
 
-  Parse.Push.send(
-  {
-    where: pushQuery,                                                                                                             
-    data: payload
-  }, 
-  { 
-    success: function() {console.log("#### PUSH OK");}, 
-    error: function(error) {console.log("#### PUSH ERROR" + error.message);}, 
-    useMasterKey: true
-  });
-
-
-  response.success('testSuccess');
+  response.success('success');
 });
