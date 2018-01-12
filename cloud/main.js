f4bc17f881352b46c219b944d7cd9b5c9359f7bb
Joniwee <33837126+Joniwee@users.noreply.github.com>
2018-01-13 00:10:26 +0200

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,36 +1,23 @@
 
-// Depends on this function: https://github.com/codepath/parse-server-example/blob/master/cloud/main.js
-Parse.Cloud.define('notifyUsers', function(request, response) {
+Parse.Cloud.define("notifyUsers", function(request, response) {
+  var query = new Parse.Query(Parse.Installation);
+  query.exists("deviceToken");
 
-  // request has 2 parameters: params passed by the client and the authorized user
-  var params = request.params;
-  var user = request.user;
-
-  // extract out the channel to send
-  var action = params.action;
-  var message = params.message;
-  var customData = params.customData;
-
-  // use to custom tweak whatever payload you wish to send
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo("deviceType", "android");
-
-  var payload = {"data": {
-      "alert": message,
-      "action": action,
-      "customdata": customData}
-                };
-
-  // Note that useMasterKey is necessary for Push notifications to succeed.
+  var payload = {
+    alert: "after save push",
+    sound: "default"
+      // ... add more here if required 
+  };
 
   Parse.Push.send({
-  where: pushQuery,      // for sending to a specific channel                                                                                                                                 
-    data: payload,
-  }, { success: function() {
-     console.log("#### PUSH OK");
-  }, error: function(error) {
-     console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-  response.success('techAlertSuccessConnecting');
+      data: payload,
+      where: query
+    }, {
+      useMasterKey: true
+    }) // useMasterKey is required currently 
+    .then(function() {
+      response.success("Push Sent!");
+    }, function(error) {
+      response.error("Error while trying to send push " + error.message);
+    });
 });
